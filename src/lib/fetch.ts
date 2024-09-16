08a8d2c9a45a4db5345eba2a6cef1461219b9f9d
David Dobondi <david.dobondi@neho.ch>
2024-09-16 23:37:53 +0300

    build fix

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -12,16 +12,15 @@ export const withCovers = async (games: Game[]) => {
         if (game.cover) coverIds.push(game.cover)
     }
     const idsString = coverIds.join(',')
-    console.log(idsString)
     const result = await api()
         .fields('*')
         .where(`id = (${idsString})`)
         .limit(UNLIMITED)
-        .get('covers')
+        .get<Cover[]>('covers')
     const gameCovers: {
         [key: number]: string
     } = {}
-    result.forEach((r: Cover) => {
+    result.forEach((r) => {
         gameCovers[r.game] = r.url ? modelImageUrl(r.url, 'cover_big_2x') : ''
     })
     
